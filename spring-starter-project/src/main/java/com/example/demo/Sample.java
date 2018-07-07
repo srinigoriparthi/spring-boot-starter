30df052ed0f0a39ddf0b3adb550126271c3ee35d
srinigoriparthi <srini.goriparthi@gmail.com>
2018-07-07 13:02:37 -0700

    Added fields.

--- spring-starter-project/src/main/java/com/example/demo/Sample.java
+++ spring-starter-project/src/main/java/com/example/demo/Sample.java
@@ -2,4 +2,37 @@ package com.example.demo;
 
 public class Sample {
 
+
+	private String ename;
+	private String department;
+	
+	/**
+	 * 
+	 * @return ename
+	 */
+	public String getEname() {
+		return ename;
+	}
+	/**
+	 * 
+	 * @param ename String
+	 */
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	/**
+	 * 
+	 * @return department
+	 */
+	public String getDepartment() {
+		return department;
+	}
+	/**
+	 * 
+	 * @param department String
+	 */
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
 }
