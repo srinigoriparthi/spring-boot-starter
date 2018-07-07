9ada39615b39ab0deda5ead6faeea6071da7a6d5
srinigoriparthi <srini.goriparthi@gmail.com>
2018-07-07 12:29:03 -0700

    GSR

--- spring-starter-project/src/main/java/com/example/demo/SpringStarterProjectApplication.java
+++ spring-starter-project/src/main/java/com/example/demo/SpringStarterProjectApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringStarterProjectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Yes Guru !!");	
+		System.out.println("Yes Rao !!");
+		System.out.println("Yes GSR !!");	
 		//The below line starts the Application.
 		SpringApplication.run(SpringStarterProjectApplication.class, args);
 	}
