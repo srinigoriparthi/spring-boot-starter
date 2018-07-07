f933f77ccdd467ff8759e44b5e6484abfeec5ff4
srinigoriparthi <srini.goriparthi@gmail.com>
2018-07-07 12:36:58 -0700

    yes user1

--- spring-starter-project/src/main/java/com/example/demo/SpringStarterProjectApplication.java
+++ spring-starter-project/src/main/java/com/example/demo/SpringStarterProjectApplication.java
@@ -10,7 +10,7 @@ public class SpringStarterProjectApplication {
 
 		System.out.println("Yes Rao !!");
 		System.out.println("Yes GSR !!");	
-
+		System.out.println("Yes User 1 !!");
 		//The below line starts the Application.
 		SpringApplication.run(SpringStarterProjectApplication.class, args);
 	}
