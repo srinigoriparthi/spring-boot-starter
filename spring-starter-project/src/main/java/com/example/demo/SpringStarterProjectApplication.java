721f9f3eb80fbc9e5c84b2b82b13a497cd6d2b97
srinigoriparthi <srini.goriparthi@gmail.com>
2018-07-07 12:03:33 -0700

    Added one more line.

--- spring-starter-project/src/main/java/com/example/demo/SpringStarterProjectApplication.java
+++ spring-starter-project/src/main/java/com/example/demo/SpringStarterProjectApplication.java
@@ -9,6 +9,7 @@ public class SpringStarterProjectApplication {
 	public static void main(String[] args) {
 		//The below line starts the Application.
 		System.out.println("Hello World !!");
+		System.out.println("Yes Guru !!");		
 		SpringApplication.run(SpringStarterProjectApplication.class, args);
 	}
 }
